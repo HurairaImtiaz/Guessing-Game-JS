32290e07d2007e743b00fb2e3eb6948c3f54109a
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-08-07 22:54:21 +0500

    Updated
    Completed the logic . Guess game is ready to play

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let maxNum = parseInt(prompt(" Hey Champ! Enter a MAximum number :"))
+let maxNum = parseInt(prompt(" Hey Champ! Enter a Maximum number :"))
 
 //
 
@@ -12,5 +12,28 @@ const targetNum = Math.floor(Math.random() * maxNum) + 1;
 
 //
 
+let yourGuess = parseInt(prompt("Enter your First Guess:"))
 
+let attempts = 1;
+
+//
+
+while (parseInt(yourGuess) !== targetNum){
+    if(yourGuess === 'q') break;
+    attempts++;
+    if (yourGuess > targetNum){
+        yourGuess = prompt("Too High! Guess again:")
+    }
+    else{
+        yourGuess = prompt("Too Low! Guess again:")
+    }
+}
+if(yourGuess === 'q'){
+    prompt("Alas! Dont loose hope . Better Luck Next Time Champ!")
+}
+else{
+    
+    prompt(`COngratulations! You Got it! it took ${attempts} to guess. THANK YOU ! SEE YOU SOON CHAMP. ` )
+    ("")
+}
 
